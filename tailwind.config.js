9d975f079d43f4087e2761fcd912dff3746f8b6f
Rajeev Kumar Verma <105263777+RajeevKumarVerma11@users.noreply.github.com>
2023-07-05 10:57:03 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  fontFamily:{
+    Whitney:["Whitney"],
+    Ginto:["Ginto"],
+    ggSans:["ggSans"],
+  },
+}
